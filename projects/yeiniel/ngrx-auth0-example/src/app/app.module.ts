cb7d2e566cb1872f08f216dec7368d1b08ffe4b2
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2020-04-13 22:56:37 -0500

    feat: add an example app

---
+++ projects/yeiniel/ngrx-auth0-example/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NgrxAuth0Module } from '@yeiniel/ngrx-auth0';
+
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+
+    NgrxAuth0Module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
