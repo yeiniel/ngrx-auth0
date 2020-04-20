41e4191ea7177663df169dd3178cd29afebcb960
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2020-04-19 23:10:57 -0500

    feat(@yeiniel/ngrx-auth0): update ngrx-auth0 module to pass configuration and setup ngrx effects

--- projects/yeiniel/ngrx-auth0/src/lib/ngrx-auth0.module.ts
+++ projects/yeiniel/ngrx-auth0/src/lib/ngrx-auth0.module.ts
@@ -1,14 +1,37 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { featureKey } from './feature-key';
 import { reducer } from './reducer';
+import { Effects } from './effects';
+import { AuthService } from './auth.service';
+import { AuthServiceOptions } from './auth-service-options';
 
 @NgModule({
   declarations: [],
   imports: [
-    StoreModule.forFeature(featureKey, reducer)
+    StoreModule.forFeature(featureKey, reducer),
+    EffectsModule.forFeature([Effects])
   ],
   exports: []
 })
-export class NgrxAuth0Module { }
+export class NgrxAuth0Module {
+
+  static forRoot(options: any): ModuleWithProviders {
+    return {
+      ngModule: NgrxAuth0Module,
+      providers: [
+        { provide: AuthServiceOptions, useValue: options },
+       AuthService
+      ]
+    }
+  }
+
+  constructor (@Optional() @SkipSelf() parentModule?: NgrxAuth0Module) {
+    if (parentModule) {
+      throw new Error(
+        'NgrxAuth0Module is already loaded. Import it in the AppModule only');
+    }
+  }
+}
