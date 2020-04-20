c96ac542ef3d0b06e2292d7e13223dce75ebc78f
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2020-04-19 22:32:07 -0500

    feat(@yeiniel/ngrx-auth0): implement state reducer

--- projects/yeiniel/ngrx-auth0/src/lib/reducer.ts
+++ projects/yeiniel/ngrx-auth0/src/lib/reducer.ts
@@ -1,10 +1,15 @@
-import {Action, createReducer} from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
 
 import { initialState } from './initial-state';
 import { State } from './state';
+import { loginCompleteAction } from './login-complete.action';
 
 /** NgRx Store reducer for the module feature */
-const actionReducer = createReducer(initialState);
+const actionReducer = createReducer(
+  initialState,
+  on(loginCompleteAction,
+    (state, { isLoggedIn }) => ({ ...state, isLoggedIn }))
+);
 
 export function reducer(state: State | undefined, action: Action) {
   return actionReducer(state, action)
