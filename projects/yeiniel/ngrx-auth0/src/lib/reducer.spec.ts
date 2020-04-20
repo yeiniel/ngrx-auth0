6ded32cdae796027b4a6461bccead68c2d4e79ab
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2020-04-19 22:31:35 -0500

    test(@yeiniel/ngrx-auth0): add test suite for the state reducer

---
+++ projects/yeiniel/ngrx-auth0/src/lib/reducer.spec.ts
@@ -0,0 +1,30 @@
+import { Action } from '@ngrx/store';
+
+import { initialState } from './initial-state';
+import { reducer } from './reducer';
+import { loginCompleteAction } from './login-complete.action';
+
+describe('@yeiniel/ngrx-auth0/reducer', () => {
+  describe('undefined action', () => {
+    it('should return the default state', () => {
+      const action = {};
+      const state = reducer(undefined, action as Action);
+
+      expect(state).toBe(initialState);
+    });
+  });
+
+  describe('loginComplete action', () => {
+    it('should set isLoggedIn to value', () => {
+      let action = loginCompleteAction({ isLoggedIn: true });
+      let state = reducer(initialState, action);
+
+      expect(state.isLoggedIn).toEqual(true);
+
+      action = loginCompleteAction({ isLoggedIn: false });
+      state = reducer(initialState, action);
+
+      expect(state.isLoggedIn).toEqual(false);
+    });
+  });
+});
