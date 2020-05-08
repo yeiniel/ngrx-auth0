5b881811fdab082606d1aa792f57a67323d35710
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2020-05-08 00:00:18 -0500

    feat(@yeiniel/ngrx-auth0): make init$ effect pass profile into loginComplete action

--- projects/yeiniel/ngrx-auth0/src/lib/effects.ts
+++ projects/yeiniel/ngrx-auth0/src/lib/effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType, OnInitEffects } from '@ngrx/effects';
 import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 import { Action, Store } from '@ngrx/store';
-import { EMPTY } from "rxjs";
+import { EMPTY, forkJoin } from "rxjs";
 
 import {
   initAction,
@@ -17,14 +17,19 @@ export class Effects implements OnInitEffects {
 
   /** Side effect for the init action
    *
-   * It merges the AuthService.isAuthenticated$ observable and map it into the
+   * It merges the AuthService.isAuthenticated$ observable and
+   * AuthService.getUser$() observable and map them both into the
    * loginComplete Action.
    */
   init$ = createEffect(() => this.actions$.pipe(
     ofType(initAction),
-    mergeMap(() => this.authService.isAuthenticated$.pipe(
-      map(isAuthenticated => loginCompleteAction({
-        isLoggedIn: isAuthenticated
+    mergeMap(() => forkJoin([
+      this.authService.isAuthenticated$,
+      this.authService.profile$
+    ]).pipe(
+      map(([isAuthenticated, profile]) => loginCompleteAction({
+        isLoggedIn: isAuthenticated,
+        profile
       })),
       catchError(() => EMPTY)
     ))
