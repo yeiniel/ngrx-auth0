a156c9da957b1b363c986b42ed3d769d48d37e3f
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2020-05-07 22:40:24 -0500

    feat(@yeiniel/ngrx-auth0): update internal loginComplete action to include user profile update
    this change will enable the module effects to send the user profile information to the store once a
    login is complete

--- projects/yeiniel/ngrx-auth0/src/lib/actions/login-complete.action.ts
+++ projects/yeiniel/ngrx-auth0/src/lib/actions/login-complete.action.ts
@@ -4,13 +4,10 @@ import { featureKey } from '../feature-key';
 
 /** Login
  *
- * This action is dispatched by the application using this module to initiate
- * the user authentication workflow.
- *
- * It does not produce any change on the stored state but it trigger side
- * effects.
+ * This action is dispatched by the init effect implemented by this module to
+ * update the feature state.
  */
 export const loginCompleteAction = createAction(
   `[${featureKey}] Login Complete`,
-  props<{ isLoggedIn: boolean }>()
+  props<{ isLoggedIn: boolean, profile?: any }>()
 );
